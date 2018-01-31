36825286ef4092fbb8107d050167eb72edec8122
Jambombe <flechebleue38@hotmail.com>
2018-01-31 18:32:13 +0100

    + Images, questions

--- sql/insert_difficiles.sql
+++ sql/insert_difficiles.sql
@@ -1,6 +1,18 @@
-﻿insert into questions_simples(ennonce, image) values ("Quel est le nom de ce champion ?", "jhin.jpg");
-insert into reponses_questions_simples values (1, "Fizz", 0);
-insert into reponses_questions_simples values (1, "Jhin", 1);
-insert into reponses_questions_simples values (1, "Jayce", 0);
-insert into reponses_questions_simples values (1, "Draven", 0);
+﻿insert into questions_simples(ennonce, image) values ("En quelle année est sorti League of Legends ?", "lol.png");
+insert into reponses_questions_simples values (1, "2007", 0);
+insert into reponses_questions_simples values (1, "2008", 0);
+insert into reponses_questions_simples values (1, "2009", 1);
+insert into reponses_questions_simples values (1, "2010", 0);
+
+insert into questions_simples(ennonce, image) values ("Quel est le prix de la Force de la Trinité ?", "trinity.png");
+insert into reponses_questions_simples values (1, "3433", 0);
+insert into reponses_questions_simples values (1, "3533", 0);
+insert into reponses_questions_simples values (1, "3633", 0);
+insert into reponses_questions_simples values (1, "3733", 1);
+
+insert into questions_simples(ennonce, image) values ("Quel est le prix de vente de l'Anneau de Doran ?", "doran_ring.png");
+insert into reponses_questions_simples values (1, "160", 1);
+insert into reponses_questions_simples values (1, "165", 0);
+insert into reponses_questions_simples values (1, "170", 0);
+insert into reponses_questions_simples values (1, "175", 0);
 
--- sql/insert_moyennes.sql
+++ sql/insert_moyennes.sql
@@ -1,6 +1,6 @@
-﻿insert into questions_simples(ennonce, image) values ("Quel est le nom de ce champion ?", "jhin.jpg");
-insert into reponses_questions_simples values (1, "Fizz", 0);
-insert into reponses_questions_simples values (1, "Jhin", 1);
-insert into reponses_questions_simples values (1, "Jayce", 0);
-insert into reponses_questions_simples values (1, "Draven", 0);
+﻿insert into questions_simples(ennonce, image) values ("Quel est le rôle principal de Twisted Fate ?", "tf.jpg");
+insert into reponses_questions_simples values (1, "Top", 0);
+insert into reponses_questions_simples values (1, "Jungle", 1);
+insert into reponses_questions_simples values (1, "Mid", 0);
+insert into reponses_questions_simples values (1, "ADC", 0);
 
