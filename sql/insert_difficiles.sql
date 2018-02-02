5237dd098a498f32ac37fb52f53453cd0f2f7343
Jambombe <flechebleue38@hotmail.com>
2018-02-02 01:21:49 +0100

    [+] Few questions for HARD difficulty

--- sql/insert_difficiles.sql
+++ sql/insert_difficiles.sql
@@ -1,18 +1,102 @@
-﻿insert into questions_simples(ennonce, image) values ("En quelle année est sorti League of Legends ?", "lol.png");
-insert into reponses_questions_simples values (1, "2007", 0);
-insert into reponses_questions_simples values (1, "2008", 0);
-insert into reponses_questions_simples values (1, "2009", 1);
-insert into reponses_questions_simples values (1, "2010", 0);
+﻿insert into questions_difficiles(id, ennonce, image) values (1, "En quelle année est sorti League of Legends ?", "lol.png");
+insert into reponses_questions_difficiles values (1, "2007", 0);
+insert into reponses_questions_difficiles values (1, "2008", 0);
+insert into reponses_questions_difficiles values (1, "2009", 1);
+insert into reponses_questions_difficiles values (1, "2010", 0);
 
-insert into questions_simples(ennonce, image) values ("Quel est le prix de la Force de la Trinité ?", "trinity.png");
-insert into reponses_questions_simples values (1, "3433", 0);
-insert into reponses_questions_simples values (1, "3533", 0);
-insert into reponses_questions_simples values (1, "3633", 0);
-insert into reponses_questions_simples values (1, "3733", 1);
+insert into questions_difficiles(id, ennonce, image) values (2, "Quel est le prix de la Force de la Trinité ?", "trinity.png");
+insert into reponses_questions_difficiles values (2, "3433", 0);
+insert into reponses_questions_difficiles values (2, "3533", 0);
+insert into reponses_questions_difficiles values (2, "3633", 0);
+insert into reponses_questions_difficiles values (2, "3733", 1);
 
-insert into questions_simples(ennonce, image) values ("Quel est le prix de vente de l'Anneau de Doran ?", "doran_ring.png");
-insert into reponses_questions_simples values (1, "160", 1);
-insert into reponses_questions_simples values (1, "165", 0);
-insert into reponses_questions_simples values (1, "170", 0);
-insert into reponses_questions_simples values (1, "175", 0);
+insert into questions_difficiles(id, ennonce, image) values (3, "Quel est le prix de vente de l'Anneau de Doran ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (3, "160", 1);
+insert into reponses_questions_difficiles values (3, "165", 0);
+insert into reponses_questions_difficiles values (3, "170", 0);
+insert into reponses_questions_difficiles values (3, "175", 0);
+
+insert into questions_difficiles(id, ennonce, image) values (4, "Quel est le vrai nom de Miss Fortune ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (4, "Laurie Fortune", 0);
+insert into reponses_questions_difficiles values (4, "Clara Fortune", 0);
+insert into reponses_questions_difficiles values (4, "Sarah Fortune", 1);
+insert into reponses_questions_difficiles values (4, "Victoria Fortune", 0);
+
+insert into questions_difficiles(id, ennonce, image) values (5, "Quel est le nom complet de Fiora ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (5, "Fiora Laurent", 1);
+insert into reponses_questions_difficiles values (5, "Fiora Thomas", 0);
+insert into reponses_questions_difficiles values (5, "Fiora Clément", 0);
+insert into reponses_questions_difficiles values (5, "Fiora Valentin", 0);
+
+insert into questions_difficiles(id, ennonce, image) values (6, "En quelle saison est sortie la relique ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (6, "Saison 4", 0);
+insert into reponses_questions_difficiles values (6, "Saison 5", 1);
+insert into reponses_questions_difficiles values (6, "Saison 6", 0);
+insert into reponses_questions_difficiles values (6, "Saison 7", 0);
+
+insert into questions_difficiles(id, ennonce, image) values (7, "Combien de temps met une plante pour ré-apparaître ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (7, "Entre 1 et 3 minutes", 0);
+insert into reponses_questions_difficiles values (7, "Entre 3 et 5 minutes", 0);
+insert into reponses_questions_difficiles values (7, "Entre 5 et 7 minutes", 1);
+insert into reponses_questions_difficiles values (7, "Entre 7 et 9 minutes", 0);
+
+insert into questions_difficiles(id, ennonce, image) values (8, "Dans quelle arbre se trouve la rune <strong>Balise zombie</strong> ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (8, "Domination", 1);
+insert into reponses_questions_difficiles values (8, "Insipration", 0);
+insert into reponses_questions_difficiles values (8, "Sorcellerie", 0);
+insert into reponses_questions_difficiles values (8, "Précision", 0);
+
+insert into questions_difficiles(id, ennonce, image) values (9, "Dans quelle arbre se trouve la rune <strong>Vitesse d\'approche</strong> ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (9, "Domination", 0);
+insert into reponses_questions_difficiles values (9, "Insipration", 1);
+insert into reponses_questions_difficiles values (9, "Sorcellerie", 0);
+insert into reponses_questions_difficiles values (9, "Précision", 0);
+
+insert into questions_difficiles(id, ennonce, image) values (10, "Quelle rune n\'existe pas ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (10, "Chasseur vorace", 0);
+insert into reponses_questions_difficiles values (10, "Carapace miroir", 0);
+insert into reponses_questions_difficiles values (10, "Baroud d\'honneur", 0);
+insert into reponses_questions_difficiles values (10, "Chaussures runiques", 1);
+
+insert into questions_difficiles(id, ennonce, image) values (11, "Que se passe-t-il quand Volibear tue Zilean ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (11, "Il gagne 2 d\'armure", 0);
+insert into reponses_questions_difficiles values (11, "Il gagne 5PV", 0);
+insert into reponses_questions_difficiles values (11, "Il gagne 11po de plus", 1);
+insert into reponses_questions_difficiles values (11, "Il gagne 7% de vitesse d\'attaque' ", 0);
+
+insert into questions_difficiles(id, ennonce, image) values (12, "À partir de quel niveau est-il possible d'acheter les objets spéciaux de Ornn ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (12, "Niveau 9", 0);
+insert into reponses_questions_difficiles values (12, "Niveau 13", 0);
+insert into reponses_questions_difficiles values (12, "Niveau 9 de Ornn", 0);
+insert into reponses_questions_difficiles values (12, "Niveau 13 de Ornn", 1);
+
+insert into questions_difficiles(id, ennonce, image) values (13, "Combien de dégâts rajoute l\'élixir de rage ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (13, "25", 0);
+insert into reponses_questions_difficiles values (13, "30", 1);
+insert into reponses_questions_difficiles values (13, "35", 0);
+insert into reponses_questions_difficiles values (13, "40", 0);
+
+insert into questions_difficiles(id, ennonce, image) values (14, "Combien de puissance rajoute l\'élixir de sorcellerie ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (14, "35", 0);
+insert into reponses_questions_difficiles values (14, "40", 0);
+insert into reponses_questions_difficiles values (14, "45", 0);
+insert into reponses_questions_difficiles values (14, "50", 1);
+
+insert into questions_difficiles(id, ennonce, image) values (15, "Combien de vie rajoute l\'élixir de fer ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (15, "200", 0);
+insert into reponses_questions_difficiles values (15, "250", 0);
+insert into reponses_questions_difficiles values (15, "300", 1);
+insert into reponses_questions_difficiles values (15, "350", 0);
+
+insert into questions_difficiles(id, ennonce, image) values (16, "Combien existe-t-il de cartes différentes (sans compter les apparences) ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (16, "4", 0);
+insert into reponses_questions_difficiles values (16, "5", 0);
+insert into reponses_questions_difficiles values (16, "6", 1);
+insert into reponses_questions_difficiles values (16, "7", 0);
+
+insert into questions_difficiles(id, ennonce, image) values (17, "À quelle minute le Herault de la faille apparaît-il ?", "doran_ring.png");
+insert into reponses_questions_difficiles values (17, "10", 1);
+insert into reponses_questions_difficiles values (17, "11", 0);
+insert into reponses_questions_difficiles values (17, "14", 0);
+insert into reponses_questions_difficiles values (17, "15", 0);
 
--- sql/insert_moyennes.sql
+++ sql/insert_moyennes.sql
@@ -1,6 +1,6 @@
 ﻿insert into questions_simples(ennonce, image) values ("Quel est le rôle principal de Twisted Fate ?", "tf.jpg");
-insert into reponses_questions_simples values (1, "Top", 0);
-insert into reponses_questions_simples values (1, "Jungle", 1);
-insert into reponses_questions_simples values (1, "Mid", 0);
-insert into reponses_questions_simples values (1, "ADC", 0);
+insert into reponses_questions_simples values (1, "", 0);
+insert into reponses_questions_simples values (1, "", 1);
+insert into reponses_questions_simples values (1, "", 0);
+insert into reponses_questions_simples values (1, "", 0);
 
